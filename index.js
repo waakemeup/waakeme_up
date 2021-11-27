0b44e79a34bf1ae8a86c0960c52acd32ee25e004
Ruby <LIU10032@163.com>
2021-11-27 16:04:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import {blue,green,yellow,underline,bgCyanBright,redBright} from 'colorette'
+import open from 'open'
+
+console.log(
+  blue('waake')+
+  green('me')+
+  yellow('_')+
+  underline('up')+
+  bgCyanBright('Here🤓')
+);
+
+setTimeout(()=>{
+  console.log(redBright("welcome to my world"));
+},5000)
+
+setTimeout(()=>{
+  open('https://www.notion.so/9d661f445ceb4ad3915afdf67054f3cc?v=944105fc4c884faf905cf65164ce6fe1')
+},10000)
